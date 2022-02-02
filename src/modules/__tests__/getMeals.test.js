c8ad3761fa360fe2c93363995dace4af8cd4f8b0
Bondok6 <kirolousbondok6@gmail.com>
2022-02-02 06:56:30 +0200

    Fix eslint errors

--- src/modules/__tests__/getMeals.test.js
+++ src/modules/__tests__/getMeals.test.js
@@ -1,12 +1,12 @@
-import { getMeals, mealsLength } from "../getMeals.js";
+import { getMeals, mealsLength } from '../getMeals.js';
 
-describe("Items (meals) Counter", () => {
-  test("Display the Number of Meals", async () => {
+describe('Items (meals) Counter', () => {
+  test('Display the Number of Meals', async () => {
     const mealsCount = await mealsLength();
     expect(mealsCount).toBe(18);
   });
 
-  test("Display the length of the items", async () => {
+  test('Display the length of the items', async () => {
     const meals = await getMeals();
     expect(meals).toHaveLength(18);
   });
