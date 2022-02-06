2a11db5d17c16209b489b7464d003955a438bce4
Bondok6 <kirolousbondok6@gmail.com>
2022-02-06 04:38:28 +0200

    Fix getMeals.test.js

--- src/modules/__tests__/getMeals.test.js
+++ src/modules/__tests__/getMeals.test.js
@@ -1,15 +1,15 @@
 import { getMeals, mealsLength } from "../getMeals.js";
 
-const category = "italian";
+const category = "Beef";
 
 describe("Items (meals) Counter", () => {
   test("Display the Number of Meals", async () => {
     const mealsCount = await mealsLength(category);
-    expect(mealsCount).toBe(18);
+    expect(mealsCount).toBe(42);
   });
 
   test("Display the length of the items", async () => {
     const meals = await getMeals(category);
-    expect(meals).toHaveLength(18);
+    expect(meals).toHaveLength(42);
   });
 });
