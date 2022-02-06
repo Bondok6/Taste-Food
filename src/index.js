6dec40cda028550d47fb12014717f4b124042e75
Bondok6 <kirolousbondok6@gmail.com>
2022-02-06 05:02:39 +0200

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './main.css';
-// import renderMeals from "./modules/renderMeals.js";
 import renderCategories from './modules/renderCategories.js';
 
 renderCategories();
-
-// renderMeals();
