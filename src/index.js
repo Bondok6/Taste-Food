c59b8cee7fda0476050f052f7145221c97810548
Bondok6 <kirolousbondok6@gmail.com>
2022-03-27 09:57:30 +0200

    Create Responsive Navigation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './main.css';
 import renderCategories from './modules/renderCategories.js';
 
+const showNav = () => {
+  document.querySelector('nav').style.opacity = 1;
+};
+
+const hideNav = () => {
+  document.querySelector('nav').style.opacity = 0;
+};
+
+document.addEventListener('click', (e) => {
+  const menuIcon = document.querySelector('.menu img');
+  return e.target === menuIcon ? showNav() : hideNav();
+});
+
+window.addEventListener('resize', (e) =>
+  e.target.innerWidth > 800 ? showNav() : hideNav()
+);
+
 renderCategories();
